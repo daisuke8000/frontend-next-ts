180c4ee31fc1ba5eaf637e64f53ee0fd7abd21ab
daisuke8000 <daisuke12108000@gmail.com>
2022-01-10 00:26:03 +0900

    menu image add

--- front-next/src/pages/index.tsx
+++ front-next/src/pages/index.tsx
@@ -8,51 +8,57 @@ import {
     Icon,
     IconProps,
 } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import IndexDef1 from '/public/svg/IndexDef1.svg';
 import IndexDef2 from '/public/svg/IndexDef2.svg';
 
-export default function IndexPage() {
+export default function Index() {
+    const router = useRouter();
     return (
-        <Container maxW={'5xl'}>
-            <Stack
-                textAlign={'center'}
-                align={'center'}
-                spacing={{ base: 8, md: 10 }}
-                py={{ base: 20, md: 28 }}>
-                <Heading
-                    fontWeight={600}
-                    fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-                    lineHeight={'110%'}>
-                    Managements{' '}
-                    <Text as={'span'} color={'purple.400'}>
-                        made easy
+        <>
+            <Container maxW={'5xl'}>
+                <Stack
+                    textAlign={'center'}
+                    align={'center'}
+                    spacing={{ base: 8, md: 10 }}
+                    py={{ base: 20, md: 28 }}>
+                    <Heading
+                        fontWeight={600}
+                        fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+                        lineHeight={'110%'}>
+                        Managements{' '}
+                        <Text as={'span'} color={'purple.400'}>
+                            made easy
+                        </Text>
+                    </Heading>
+                    <Text color={'gray.500'} maxW={'3xl'}>
+                            All calculation management should be done in an application.
+                            It is the easiest and quickest way to solve your work issues.
                     </Text>
-                </Heading>
-                <Text color={'gray.500'} maxW={'3xl'}>
-                        All calculation management should be done in an application.
-                        It is the easiest and quickest way to solve your work issues.
-                </Text>
-                <Stack spacing={6} direction={'row'}>
-                    <Button
-                        rounded={'full'}
-                        px={6}
-                        colorScheme={'purple'}
-                        bg={'purple.400'}
-                        _hover={{ bg: 'purple.500' }}>
-                        Get started
-                    </Button>
-                    <Button rounded={'full'} px={6}>
-                        Learn more
-                    </Button>
+                    <Stack spacing={6} direction={'row'}>
+                        <Button
+                            rounded={'full'}
+                            px={6}
+                            colorScheme={'purple'}
+                            bg={'purple.400'}
+                            _hover={{ bg: 'purple.500' }}
+                            onClick={() => router.push("/auth/auth")}
+                        >
+                            Get started
+                        </Button>
+                        <Button rounded={'full'} px={6}>
+                            Learn more
+                        </Button>
+                    </Stack>
+                    <Flex w={'full'}>
+                        <Illustration
+                            height={{ sm: '24rem', lg: '28rem' }}
+                            mt={{ base: 12, sm: 16 }}
+                        />
+                    </Flex>
                 </Stack>
-                <Flex w={'full'}>
-                    <Illustration
-                        height={{ sm: '24rem', lg: '28rem' }}
-                        mt={{ base: 12, sm: 16 }}
-                    />
-                </Flex>
-            </Stack>
-        </Container>
+            </Container>
+        </>
     );
 }
 
