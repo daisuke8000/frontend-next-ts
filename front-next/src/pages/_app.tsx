407bdc3d761fff7d08384ad2767788517e9473f8
daisuke8000 <daisuke12108000@gmail.com>
2022-01-09 12:26:17 +0900

    index page custom

--- front-next/src/pages/_app.tsx
+++ front-next/src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from 'next/app'
+import { extendTheme, ThemeConfig } from '@chakra-ui/react'
+
+const theme: ThemeConfig = extendTheme({
+    styles: {
+        global:{
+            "html, body":{
+                bg: 'pink.50',
+            },
+        },
+    },
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ChakraProvider>
-        <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps}/>
       </ChakraProvider>
   )
 }
